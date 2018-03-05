ef134d6a9bd266a995e3179f87efafcba99fbbd4
Thom Jonsson Persson <36730660+ThomJPersson@users.noreply.github.com>
2018-03-05 17:15:59 +0100

    Add files via upload

---
+++ StrobeLite/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  StrobeLite
+//
+//  Created by Thom Jonsson Persson on 2018-02-26.
+//  Copyright © 2018 Thom Jonsson Persson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+   
+    print("Lol")
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
